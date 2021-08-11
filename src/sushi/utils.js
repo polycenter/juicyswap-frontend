6ad47b3942ecf371f2f82c8e30075560ca71bd9b
PolyCenter <86454867+polycenter@users.noreply.github.com>
2021-08-11 15:34:20 +0700

    up

--- src/sushi/utils.js
+++ src/sushi/utils.js
@@ -57,7 +57,7 @@ export const getFarms = (sushi) => {
           tokenAddress,
           tokenSymbol,
           tokenContract,
-          earnToken: 'JUICY',
+          earnToken: 'LDOGE',
           earnTokenAddress: sushi.contracts.sushi.options.address,
           icon,
         }),
